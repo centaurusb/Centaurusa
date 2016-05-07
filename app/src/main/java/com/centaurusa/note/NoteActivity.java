f234818912fc24eee39cbf291d3bf7c428bf666b
Swin Zhang <heatre@163.com>
2016-05-07 20:30:17 +0800

    添加侧滑导航和toolbar

--- app/src/main/java/com/centaurusa/note/NoteActivity.java
+++ app/src/main/java/com/centaurusa/note/NoteActivity.java
@@ -3,19 +3,28 @@ package com.centaurusa.note;
 import com.centaurusa.www.centaurusa.R;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 
 public class NoteActivity extends AppCompatActivity {
+    private static final String TAG = "NoteActivity.Class";
+    private Toolbar mToolbar ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+
+        //set the toolbar
+        mToolbar = (Toolbar)findViewById(R.id.note_toolbar);
+        setSupportActionBar(mToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
